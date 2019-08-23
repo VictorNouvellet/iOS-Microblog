22ad8ae5b582cbb3201e0476e6a1d71fb21b0a7d
Victor Nouvellet <victor.nouvellet@gmail.com>
2019-08-24 10:28:11 +1200

    Initial commit

---
+++ Microblog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Microblog
+//
+//  Created by Victor on 24/08/2019.
+//  Copyright © 2019 Victor Nouvellet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
