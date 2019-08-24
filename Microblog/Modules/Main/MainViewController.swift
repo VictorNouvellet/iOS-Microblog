843f6882ef1e6199807080adecd4ad42413c361a
Victor Nouvellet <victor.nouvellet@gmail.com>
2019-08-24 15:53:44 +1200

    Improve workspace structure
    - Improve workspace structure
    - Add icon

--- Microblog/Modules/Main/MainViewController.swift
+++ Microblog/Modules/Main/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Microblog
 //
 //  Created by Victor on 24/08/2019.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
